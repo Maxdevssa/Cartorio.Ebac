15cac17d311e08723ed4d73c297c147beb0cd8c9
Bruno Silveira <brunosilveirassa@gmail.com>
2024-06-21 13:53:34 -0300

    Add opc sair do sistema
    Upgrade com opção de sair do sistema para o usuário

--- cartorio1.c
+++ cartorio1.c
@@ -147,7 +147,8 @@ int main()
   		printf("\t1 - Registrar nomes\n");
   		printf("\t2 - Consultar nomes\n");
   		printf("\t3 - Deletar nomes\n\n"); 
-  		printf("Opcao:"); // Fim do menu
+  		printf("\4 - Sair do sistema\n\n");
+		printf("Opcao:"); // Fim do menu
   
   		scanf("%d",&opcao); // Armazenando a escolha do usuário
   
@@ -167,6 +168,10 @@ int main()
   			deletar();	
   			break;  	
   			
+  			case 4:
+  			printf("Obrigado por utilizar o sistema!\n");
+			return 0;  	
+  			break;  	
 						    			  			
   			default:
   			printf("Essa opcao nao esta disponivel\n");
